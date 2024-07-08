be742c6443a69ad03c21daf12e95bf58523f988c
manager <DELL@DESKTOP-K1JR4PD>
2024-07-08 23:54:23 +0530

    commit message

---
+++ javacourse/src/com/tnsif/superkeyword/Superex.java
@@ -0,0 +1,16 @@
+package com.tnsif.superkeyword;
+
+public class Superex {
+	
+		
+		int a=20;
+		
+		void drinking() {
+			System.out.println("coffe");
+		}
+		Superex(){
+			System.out.println("hello");
+		}
+
+
+}
