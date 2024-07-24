6d86d784178d9fbb63bd125838cf0bb999743473
manager <DELL@DESKTOP-K1JR4PD>
2024-07-24 14:37:01 +0530

    commit

---
+++ javacourse/src/com/tnsif/encapsulation/Encapsdemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.encapsulation;
+
+public class Encapsdemo {
+	public static void main(String[] args) {
+		Encapsulationdemo e=new Encapsulationdemo();
+		e.serialnumber=1;
+		e.name="jubbu";
+		e.age=20;
+		
+		e.show();
+	}
+
+}
